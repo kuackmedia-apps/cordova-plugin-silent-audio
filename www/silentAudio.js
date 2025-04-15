f90b52c10a243af6dc1d03268429578fe9501cba
mlerena <manuellerena@gmail.com>
2025-04-15 11:01:18 -0300

    Fix js

--- www/silentAudio.js
+++ www/silentAudio.js
@@ -1,13 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.startSilentAudio = function (success, error) {
-    exec(success, error, 'SilentAudioPlugin', 'startSilentAudio', []);
+exports.startSilentAudio = function(success, error) {
+    exec(success, error, 'CDVSilentAudioPlugin', 'startSilentAudio', []);
 };
 
-exports.pauseSilentAudio = function (success, error) {
-    exec(success, error, 'SilentAudioPlugin', 'pauseSilentAudio', []);
+exports.pauseSilentAudio = function(success, error) {
+    exec(success, error, 'CDVSilentAudioPlugin', 'pauseSilentAudio', []);
 };
 
-exports.stopSilentAudio = function (success, error) {
-    exec(success, error, 'SilentAudioPlugin', 'stopSilentAudio', []);
+exports.stopSilentAudio = function(success, error) {
+    exec(success, error, 'CDVSilentAudioPlugin', 'stopSilentAudio', []);
 };
