e08c898a49323776a0bdb4058a883d7b1863b3a5
mlerena <manuellerena@gmail.com>
2025-04-16 16:05:47 -0300

    Add audio interruption.

--- src/ios/CDVSilentAudioPlugin.m
+++ src/ios/CDVSilentAudioPlugin.m
@@ -8,6 +8,34 @@
 
 - (void)pluginInitialize {
     [super pluginInitialize];
+
+    // Observador claramente para interrupciones de audio
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(audioSessionInterrupted:)
+                                                 name:AVAudioSessionInterruptionNotification
+                                               object:nil];
+}
+
+- (void)audioSessionInterrupted:(NSNotification *)notification {
+    NSDictionary *info = notification.userInfo;
+    AVAudioSessionInterruptionType type = [info[AVAudioSessionInterruptionTypeKey] unsignedIntegerValue];
+
+    if (type == AVAudioSessionInterruptionTypeBegan) {
+        NSLog(@"🔔 Interrupción de audio iniciada, pausando claramente audio silencioso.");
+        [self.audioPlayer pause];
+
+    } else if (type == AVAudioSessionInterruptionTypeEnded) {
+        NSError *error = nil;
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:&error];
+        [[AVAudioSession sharedInstance] setActive:YES error:&error];
+
+        if (error == nil && self.audioPlayer) {
+            NSLog(@"🔔 Interrupción claramente terminada, reanudando claramente audio silencioso.");
+            [self.audioPlayer play];
+        } else {
+            NSLog(@"❌ Error reactivando audio tras interrupción: %@", error);
+        }
+    }
 }
 
 - (void)startSilentAudio:(CDVInvokedUrlCommand*)command {
@@ -19,13 +47,19 @@
         if (!self.audioPlayer) {
             NSString *soundPath = [[NSBundle mainBundle] pathForResource:@"silent" ofType:@"mp3"];
             NSURL *soundURL = [NSURL fileURLWithPath:soundPath];
-            
+
             self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:&error];
             self.audioPlayer.numberOfLoops = -1;
             self.audioPlayer.volume = 0.0;
+            [self.audioPlayer prepareToPlay];
         }
 
-        [self.audioPlayer play];
+        if (!error && self.audioPlayer) {
+            [self.audioPlayer play];
+            NSLog(@"✅ Silent audio claramente iniciado.");
+        } else {
+            NSLog(@"❌ Error iniciando silent audio: %@", error);
+        }
 
         CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
@@ -36,6 +70,7 @@
     [self.commandDelegate runInBackground:^{
         if (self.audioPlayer && self.audioPlayer.isPlaying) {
             [self.audioPlayer pause];
+            NSLog(@"🔔 Audio silencioso pausado.");
         }
 
         CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
@@ -49,6 +84,7 @@
             [self.audioPlayer stop];
             self.audioPlayer = nil;
             [[AVAudioSession sharedInstance] setActive:NO error:nil];
+            NSLog(@"🔔 Audio silencioso claramente detenido.");
         }
 
         CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
